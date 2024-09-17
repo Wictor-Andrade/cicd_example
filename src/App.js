43d1708e41a85990123b67cb8b99795f94b8f3c7
“Wictor <wictorrafael@live.com>
2024-09-17 20:35:45 -0300

    oi para a sabinosa

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Um oi para meu amigo pickles</h1>
+      <h1>Um oi para meu amigo conrado, e para minha professora sabinosa</h1>
     </div>
   );
 }
